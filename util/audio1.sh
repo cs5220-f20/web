96a1eeed7b4c07df738cddfc702004e4ad3b7e3e
David Bindel <bindel@cs.cornell.edu>
2020-09-01 21:07:22 -0400

    Added audio transfer script.

---
+++ util/audio1.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Take the downloaded audio and upload it to the class directory.
+
+scp ~/Downloads/audio.zip cslinux:/courses/cs5220/2020fa/lec/
+ssh cslinux "mkdir -p /courses/cs5220/2020fa/lec/$1 && cd /courses/cs5220/2020fa/lec/$1 && unzip ../audio.zip && rm ../audio.zip"
