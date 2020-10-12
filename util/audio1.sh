5e58ac68e6a096dc842208a8936015a433c973de
David Bindel <bindel@cs.cornell.edu>
2020-10-12 07:08:41 -0400

    Narrated shared memory / OpenMP lectures.

--- util/audio1.sh
+++ util/audio1.sh
@@ -4,3 +4,4 @@
 
 scp ~/Downloads/audio.zip cslinux:/courses/cs5220/2020fa/lec/
 ssh cslinux "mkdir -p /courses/cs5220/2020fa/lec/$1 && cd /courses/cs5220/2020fa/lec/$1 && unzip ../audio.zip && rm ../audio.zip"
+mv ~/Downloads/audio.zip ~/Downloads/$1.zip
